924a21b05e75c4d2b762a10dcf8ca58edcc1b43b
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2018-08-24 10:37:19 -0400

    Update OneLiners.bat

--- OneLiners.bat
+++ OneLiners.bat
@@ -24,3 +24,5 @@ net use z: /delete &^
 net use z: \\server1\share1 /persistent:yes &^
 net use y: /delete &^
 net use y: \\server2\share2 /persistent:yes
+
+FOR %s IN (srv1,srv2) DO (copy veyon-4.1.1.0-win32-setup.exe "\\%s\c$\program files\ardy\" && ..\pstools\psexec \\%s "c:\program files\ardy\veyon-4.1.1.0-win32-setup.exe" /S /NoMaster 
