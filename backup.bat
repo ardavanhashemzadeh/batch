97302529a85269fc3e2d5023c7d1f216fe76cf39
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-08-05 23:09:48 -0400

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,2 +1,4 @@
 7z a -t7z -mhe=on -r -p<password> backup-%DATE% d:\temp
 no, not this
+try this:
+https://superuser.com/questions/340046/creating-an-archive-from-a-directory-without-the-directory-name-being-added-to-t/340062
