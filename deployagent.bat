09415132b02f2ea8a20be0ba1efff69d9b9e170e
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-08-23 13:53:43 -0400

    Create deployagent.bat

---
+++ deployagent.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+REM Ardavan Hashemzadeh
+REM 08232019
+
+SET EnableDelayedExpansion
+
+set agent=\\server\share\agent_install.exe
+set tdsrp=\\server\share\tdsrp.bat
+
+FOR %%h IN (%*) DO (
+
+MKDIR \\%1\c$\temp
+COPY %agent% \\%1\c$\temp\
+COPY %tdsrp% \\%1\c$\windows
+PSEXEC \\%1 CMD /C "tdsrp \temp\agent_install.exe/silent"
+)
