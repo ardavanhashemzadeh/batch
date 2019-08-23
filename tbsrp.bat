d9e211a2bdb1cfccd731f7e4e5382b31ff2a9ebe
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-08-23 13:42:39 -0400

    Update tbsrp.bat

--- tbsrp.bat
+++ tbsrp.bat
@@ -3,4 +3,4 @@ REM Ardavan Hashemzadeh
 REM Created on 08232019
 REM Place in whitelisted path
 REM then run with argument
-reg copy HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer.old && reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer /f && cmd /c "%*" & reg copy HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer.old HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer && reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer.old /f
+reg copy HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer.old && reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer /f && cmd /c "%*" & reg copy HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer.old HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer && reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Safer.old /f & gpupdate/force
